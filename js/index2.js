28d8089f0d5921cac5b858f207e14029530eac0e
Isaac <isaacgc08@gmail.com>
2021-01-15 02:51:59 -0800

    [NEW] Added shadows, Build animation addd

--- js/index2.js
+++ js/index2.js
@@ -1,14 +1,39 @@
 const projectScreen = document.querySelector(".v2screen");
+const infoWrap = document.querySelector(".infoWrap");
+const projectSelect = document.querySelector(".v2projectSelect");
 const movieHunt = "https://moviehunt-gc.herokuapp.com/client/";
 const pokeDex = "https://gcnomore.github.io/pokedex/";
 const kokoaTalk = "https://github.com/GCnomore/chatApp";
 const toDo = "https://gcnomore.github.io/todoApp/";
 const watsPoppin = "https://gcnomore.github.io/watspoppin/";
 
-window.document.addEventListener("click", (e) => {
-  const projectName = e.target.className.slice(10);
-  projectName == "movieHunt" && projectScreen.setAttribute("src", movieHunt);
-  projectName == "pokeDex" && projectScreen.setAttribute("src", pokeDex);
-  projectName == "toDo" && projectScreen.setAttribute("src", toDo);
-  projectName == "watsPoppin" && projectScreen.setAttribute("src", watsPoppin);
+window.document.addEventListener("click", (e) => {});
+
+projectSelect.addEventListener("click", (e) => {
+  const infoName = e.target.className.slice(10);
+  const infoChildren = Array.from(infoWrap.children);
+  infoChildren.forEach((item) => {
+    item.classList[0].slice(2) == `${infoName}Info` &&
+      showInfo(infoChildren, item);
+  });
 });
+
+infoWrap.addEventListener("click", (e) => {
+  showProject(e);
+});
+
+const showInfo = (children, item) => {
+  console.log(children, item);
+  children.forEach((item) => item.classList.add("hide"));
+  item.classList.remove("hide");
+};
+
+const showProject = (e) => {
+  const projectName = e.target.className;
+  projectName == "see_movieHunt" &&
+    projectScreen.setAttribute("src", movieHunt);
+  projectName == "see_pokeDex" && projectScreen.setAttribute("src", pokeDex);
+  projectName == "see_toDo" && projectScreen.setAttribute("src", toDo);
+  projectName == "see_watsPoppin" &&
+    projectScreen.setAttribute("src", watsPoppin);
+};
