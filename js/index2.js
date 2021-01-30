cb599295e4222c4a1f5a29ebed057aafb7b7b670
GC <isaacgc08@gmail.com>
2021-01-29 18:08:41 -0800

    [UPDATE] Added some animation on index2

--- js/index2.js
+++ js/index2.js
@@ -1,48 +1,53 @@
-const projectScreen = document.querySelector(".v2screen");
-const infoWrap = document.querySelector(".infoWrap");
-const projectSelect = document.querySelector(".v2projectSelect");
+const projectScreen = document.querySelector('.v2screen');
+const infoWrap = document.querySelector('.infoWrap');
+const projectSelect = document.querySelector('.v2projectSelect');
+const _screen = document.querySelector('.test1');
 
 const projects = [
   {
-    name: "movieHunt",
-    url: "https://moviehunt-gc.herokuapp.com/client/",
+    name: 'movieHunt',
+    url: 'https://moviehunt-gc.herokuapp.com/client/',
   },
   {
-    name: "pokeDex",
-    url: "https://gcnomore.github.io/pokedex/",
+    name: 'pokeDex',
+    url: 'https://gcnomore.github.io/pokedex/',
   },
   {
-    name: "kokoaTalk",
-    url: "https://github.com/GCnomore/chatApp",
+    name: 'kokoaTalk',
+    url: 'https://github.com/GCnomore/chatApp',
   },
   {
-    name: "toDo",
-    url: "https://gcnomore.github.io/todoApp/",
+    name: 'toDo',
+    url: 'https://gcnomore.github.io/todoApp/',
   },
   {
-    name: "watsPoppin",
-    url: "https://gcnomore.github.io/watspoppin/",
+    name: 'watsPoppin',
+    url: 'https://gcnomore.github.io/watspoppin/',
   },
 ];
 
 // Show project info when the title is clicked
-projectSelect.addEventListener("click", (e) => {
+projectSelect.addEventListener('click', (e) => {
   const infoName = e.target.className.slice(10);
   const infoChildren = Array.from(infoWrap.children);
   infoChildren.forEach((item) => {
     item.classList[0].slice(2) == `${infoName}Info` &&
       showInfo(infoChildren, item);
   });
+  _screen.classList.remove('hidden');
+  _screen.classList.add('expand');
 });
 
-infoWrap.addEventListener("click", (e) => {
+infoWrap.addEventListener('click', (e) => {
   showProject(e);
 });
 
 const showInfo = (children, item) => {
-  console.log(children, item);
-  children.forEach((item) => item.classList.add("hide"));
-  item.classList.remove("hide");
+  children.forEach((item) => {
+    item.classList.add('fadeIn');
+    item.classList.add('hide');
+  });
+  item.classList.remove('hide');
 };
 
 // Show project on iframe screen when show app is clicked
@@ -52,20 +57,20 @@ const showProject = (e) => {
     return targetProject === `see_${item.name}`;
   });
 
-  if (targetProject.includes("see")) {
+  if (targetProject.includes('see')) {
     const iconWrap = document.querySelector(`.v2${match[0].name}IconsWrap`);
     const iconChildren = Array.from(iconWrap.children);
 
     // Related icons animation
     iconChildren.forEach((item, index) => {
-      item.classList.remove("build");
-      projectScreen.removeAttribute("src");
+      item.classList.remove('build');
+      projectScreen.removeAttribute('src');
       setTimeout(() => {
-        item.classList.add("build");
+        item.classList.add('build');
       }, 0 + index * 200);
     });
     setTimeout(() => {
-      projectScreen.setAttribute("src", match[0].url);
+      projectScreen.setAttribute('src', match[0].url);
     }, 2500);
   }
 };
